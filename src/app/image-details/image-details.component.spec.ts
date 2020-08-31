e094bacef3a677af7b20e9492281687a25c12e52
sanmartin98 <camilo9856@hotmail.com>
2020-08-31 00:12:56 -0500

    image detail component

--- src/app/image-details/image-details.component.spec.ts
+++ src/app/image-details/image-details.component.spec.ts
@@ -1,25 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
 import { ImageDetailComponent } from './image-details.component';
+import { ImageService } from '../image.service';
 
-xdescribe('ImageDetailsComponent', () => {
+describe('ImageDetailsComponent', () => {
   let component: ImageDetailComponent;
   let fixture: ComponentFixture<ImageDetailComponent>;
+  let mockService: ImageService = new ImageService();
+  let spy:any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ImageDetailComponent ]
+      declarations: [ ImageDetailComponent ],
+      providers:[
+        {
+          provide:ActivatedRoute,
+          useValue:{
+            snapshot:{
+              params:()=>1,
+            },
+          },
+        },
+        {provide:ImageService,useValue:mockService}
+      ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    spy = spyOn(mockService,'getImage').and.returnValue({ "id": 1, "brand": "perro", "url": "assets/images/perro1.jpg" });
+
     fixture = TestBed.createComponent(ImageDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  describe('ngOnInit', () =>{
+    it('Cuando la imagen existe debe retornar el json de la imagen', () => {
+      expect(component.image).toEqual({ "id": 1, "brand": "perro", "url": "assets/images/perro1.jpg" });
+    });
+
+    it('Debe crear el componente', () => {
+      expect(component).toBeTruthy();
+    });
+  })
+  
+  describe('fixture', () => {
+    it('Cuando se carga la vista, debe existir un elemento contenedor de imagenes con la clase img-container', () => {
+      expect(fixture.nativeElement.querySelector('.img-container')).toBeDefined();
+    });
+  })
 });

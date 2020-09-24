42dcda7d155ff837681063e60ee2244050f3f55d
Santiago Castañeda Restrepo <santixdim100@gmail.com>
2020-09-23 22:14:02 -0500

    Pruebas unitarias filterimages

--- src/app/filterimages.pipe.spec.ts
+++ src/app/filterimages.pipe.spec.ts
@@ -9,7 +9,7 @@ describe('FilterimagesPipe', () => {
     });
   });
   
-  describe('Filtro perro', () => {
+  describe('Escenarios exitosos filtro', () =>{
     it('Cuando se filtra por perro se deben eliminar los elementos de la lista que tenga el atributo brand=gato', () => {
       expect(pipe.transform([{ "id": 1, "brand": "perro", "url": "assets/images/perro1.jpg" },    
       { "id": 2, "brand": "perro", "url": "assets/images/perro2.jpg" },
@@ -20,9 +20,7 @@ describe('FilterimagesPipe', () => {
       { "id": 2, "brand": "perro", "url": "assets/images/perro2.jpg" },
       { "id": 5, "brand": "perro", "url": "assets/images/perro3.jpg" }]);
     });
-  });
 
-  describe('Filtro gato', () => {
     it('Cuando se filtra por gato se deben eliminar los elementos de la lista que tenga el atributo brand=perro', () => {
       expect(pipe.transform([{ "id": 1, "brand": "perro", "url": "assets/images/perro1.jpg" },    
       { "id": 2, "brand": "perro", "url": "assets/images/perro2.jpg" },
@@ -32,9 +30,7 @@ describe('FilterimagesPipe', () => {
       'gato')).toEqual([{ "id": 3, "brand": "gato", "url": "assets/images/gato1.jpg" },
       { "id": 4, "brand": "gato", "url": "assets/images/gato2.jpeg" }]);
     });
-  });
 
-  describe('Filtro all', () => {
     it('Cuando se filtra por all se debe devolver la lista completa', () => {
       expect(pipe.transform([{ "id": 1, "brand": "perro", "url": "assets/images/perro1.jpg" },    
       { "id": 2, "brand": "perro", "url": "assets/images/perro2.jpg" },
@@ -48,5 +44,24 @@ describe('FilterimagesPipe', () => {
       { "id": 5, "brand": "perro", "url": "assets/images/perro3.jpg" }]);
     });
   });
-  
+
+  describe('Escenarios fallidos filtro', () =>{
+    it('Cuando se filtra por un animal que no esta en la lista (por ejemplo pato) se debe retornar la lista vacia', () => {
+      expect(pipe.transform([{ "id": 1, "brand": "perro", "url": "assets/images/perro1.jpg" },    
+      { "id": 2, "brand": "perro", "url": "assets/images/perro2.jpg" },
+      { "id": 3, "brand": "gato", "url": "assets/images/gato1.jpg" },
+      { "id": 4, "brand": "gato", "url": "assets/images/gato2.jpeg" },
+      { "id": 5, "brand": "perro", "url": "assets/images/perro3.jpg" }], 
+      'pato')).toEqual([ ]);
+    });
+
+    it('Cuando no se especifica el animal por el cual se quiere filtrar se debe retornar la lista vacia', () => {
+      expect(pipe.transform([{ "id": 1, "brand": "perro", "url": "assets/images/perro1.jpg" },    
+      { "id": 2, "brand": "perro", "url": "assets/images/perro2.jpg" },
+      { "id": 3, "brand": "gato", "url": "assets/images/gato1.jpg" },
+      { "id": 4, "brand": "gato", "url": "assets/images/gato2.jpeg" },
+      { "id": 5, "brand": "perro", "url": "assets/images/perro3.jpg" }], 
+      '')).toEqual([ ]);
+    });
+  });
 });

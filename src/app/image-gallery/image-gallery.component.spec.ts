ef0afe5b7ca8e309a33f6eaba2405aebceecc63b
andresarangopro <andresarangopro@gmail.com>
2020-09-23 22:15:07 -0500

    pruebas unitarias image-gallery

--- src/app/image-gallery/image-gallery.component.spec.ts
+++ src/app/image-gallery/image-gallery.component.spec.ts
@@ -10,7 +10,6 @@ describe('ImageGalleryComponent', () => {
   let component: GalleryComponent;
   let fixture: ComponentFixture<GalleryComponent>;
   let imgService: ImageService = new ImageService();
-  let spy:any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -23,7 +22,6 @@ describe('ImageGalleryComponent', () => {
   }));
 
   beforeEach(() => {
-    //spy = spyOn(imgService,'getImages').and.returnValues([{ "id": 1, "brand": "perro", "url": "assets/images/perro1.jpg" },{ "id": 5, "brand": "perro", "url": "assets/images/perro3.jpg" }])
     fixture = TestBed.createComponent(GalleryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -35,17 +33,16 @@ describe('ImageGalleryComponent', () => {
     });
   })
   
-  
   describe('Validacion Lista Mascotas',()=>{
-    it('Cuando se obtenga la lista se debe cargar la informacion en el orden estipulado', () => {
+    it('Cuando se obtenga la lista cada elemento debe tener su respectiva imagen', () => {
       let uris = ['assets/images/perro1.jpg','assets/images/perro2.jpg','assets/images/gato1.jpg','assets/images/gato2.jpeg','assets/images/perro3.jpg'];
       let resp = imgService.getImages();
       for (let index = 0; index < resp.length; index++) {
         const element = resp[index];
         expect(element.url).toBe(uris[index]);
       }
-      
     });
+
     it('Cuando se obtenga la lista ningun elemento puede tener un atributo null', () => {
       let resp = imgService.getImages();
       for (let index = 0; index < resp.length; index++) {
@@ -54,8 +51,8 @@ describe('ImageGalleryComponent', () => {
         expect(element.brand).not.toBeNull();
         expect(element.url).not.toBeNull();
       }
-      
     });
+
     it('Cuando se cargue el componente debe cargar la lista completa de mascotas', () => {
       let resp = imgService.getImages();
       expect(resp.length).toBe(5);
@@ -72,8 +69,7 @@ describe('ImageGalleryComponent', () => {
           isNotCatOrDog = true;
         }
         expect(isNotCatOrDog).toBeFalse();
-      }
-      
+      }  
     });
   });
 
@@ -81,6 +77,18 @@ describe('ImageGalleryComponent', () => {
     it('Cuando se carga la vista, debe existir un elemento contenedor de imagenes con la clase img', () => {
       expect(fixture.nativeElement.querySelector('.img')).toBeDefined();
     });
-  })
+
+    it('Cuando se carga la vista, debe existir 1 boton para los filtros con el nombre All', () => {
+      expect(fixture.nativeElement.querySelector('#btnAll').textContent.trim()).toBe("All");
+    });
+
+    it('Cuando se carga la vista, debe existir 1 boton para los filtros con el nombre Perro', () => {
+      expect(fixture.nativeElement.querySelector('#btnPerro').textContent.trim()).toBe("Perro");
+    });
+
+    it('Cuando se carga la vista, debe existir 1 boton para los filtros con el nombre Gato', () => {
+      expect(fixture.nativeElement.querySelector('#btnGato').textContent.trim()).toBe("Gato");
+    });
+  });
 
 });
